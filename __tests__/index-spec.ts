85be056af02a758dd9d7276b474d4ee2224653c5
calidion <calidion@gmail.com>
2020-01-05 22:20:40 +0800

    added aex test

--- __tests__/index-spec.ts
+++ __tests__/index-spec.ts
@@ -1,6 +1,8 @@
 
+import { Aex } from "@aex/core";
 import * as path from "path";
 import { createTypeorm } from '../src/index';
+import { responseText } from "./util/test";
 
 test('Should connect to sqlite3', async () => {
   const options = {
@@ -78,3 +80,35 @@ test('Should throw Exception when no directory found!', async () => {
   }
   expect(catched).toBeTruthy();
 });
+
+test('Should work with aex', async () => {
+  const options = {
+    database: path.resolve(__dirname, "./store/project.db"),
+    synchronize: true,
+    type: "sqlite",
+  }
+  const middleware = await createTypeorm(path.resolve(__dirname, "./models/"), options);
+
+  const aex = new Aex();
+
+  aex.use(middleware);
+  // tslint:disable-next-line:variable-name
+  aex.use(async (_req:any, res, scope: any) => {
+    const { connection, models } = scope.outer.typeorm;
+
+    expect(connection).toBeTruthy();
+    expect(models.Photo);
+    const User = models.User;
+    const user = new User();
+    expect(user).toBeTruthy();
+    user.age = 100;
+    user.firstName = "hello";
+    user.lastName = "world!";
+    await user.save();
+    expect(user.id).toBeTruthy();
+    await connection.close();
+    res.end("ok");
+  });
+  await responseText(aex, "ok");
+});
+
